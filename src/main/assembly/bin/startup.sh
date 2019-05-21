ca42de02e4d309806e89187e532e9faed9db1ad9
qutl <qutl@nextop.cn>
2019-05-21 11:04:10 +0800

    z

--- src/main/assembly/bin/startup.sh
+++ src/main/assembly/bin/startup.sh
@@ -3,6 +3,6 @@ export BASE_PATH=/app/apple
 export APPLICATION="apple"
 export APPLICATION_JAR="${APPLICATION}-0.0.1-SNAPSHOT.jar"
 
-export JAVA_OPT="-server -XX:+HeapDumpOnOutOfMemoryError -XX:+ExitOnOutOfMemoryError -XX:+AggressiveOpts -XX:+UseG1GC -XX:NewRatio=3 -verbose:gc -XX:+PrintGCCause -XX:+PrintGCDateStamps -XX:+PrintGCApplicationStoppedTime -Djava.net.preferIPv4Stack=true -XX:MaxDirectMemorySize=128M & >> ${APP_HOME}/log/${APP_NAME}.out 2>&1
+export JAVA_OPT="-server -XX:+HeapDumpOnOutOfMemoryError -XX:+ExitOnOutOfMemoryError -XX:+AggressiveOpts -XX:+UseG1GC -XX:NewRatio=3 -verbose:gc -XX:+PrintGCCause -XX:+PrintGCDateStamps -XX:+PrintGCApplicationStoppedTime -Djava.net.preferIPv4Stack=true -XX:MaxDirectMemorySize=256M & >> ${APP_HOME}/log/${APP_NAME}.out 2>&1
 
 nohup java ${JAVA_OPT} -jar ${BASE_PATH}/boot/${APPLICATION_JAR} --spring.config.location=${CONFIG_DIR} & >> ${BASE_PATH}/log/${APPLICATION}.out 2>&1
