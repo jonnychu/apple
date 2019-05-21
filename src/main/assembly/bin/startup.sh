e46da8c48c615718d0110bf92fe23bd4fb1d646e
qutl <qutl@nextop.cn>
2019-05-21 11:49:04 +0800

    z

--- src/main/assembly/bin/startup.sh
+++ src/main/assembly/bin/startup.sh
@@ -2,6 +2,7 @@
 export BASE_PATH=/app/apple
 export APPLICATION="apple"
 export APPLICATION_JAR="${APPLICATION}-0.0.1-SNAPSHOT.jar"
+export CONFIG_DIR=${BASE_PATH}/config
 
 export JAVA_OPT="-server -XX:+HeapDumpOnOutOfMemoryError -XX:+ExitOnOutOfMemoryError -XX:+AggressiveOpts -XX:+UseG1GC -XX:NewRatio=3 -verbose:gc -XX:+PrintGCCause -XX:+PrintGCDateStamps -XX:+PrintGCApplicationStoppedTime -Djava.net.preferIPv4Stack=true -XX:MaxDirectMemorySize=256M"
 
