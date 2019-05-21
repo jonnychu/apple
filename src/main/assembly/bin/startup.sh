932ced4a1e37ef7a2e14f5bee7245743607f3297
qutl <qutl@nextop.cn>
2019-05-21 10:41:29 +0800

    z

--- src/main/assembly/bin/startup.sh
+++ src/main/assembly/bin/startup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-export BASE_PATH=/app/resfulapi
-export APPLICATION="restful-api"
-export APPLICATION_JAR="${APPLICATION}.jar"
+export BASE_PATH=/app/apple
+export APPLICATION="apple"
+export APPLICATION_JAR="${APPLICATION}-0.0.1-SNAPSHOT.jar"
+export LOG_PATH="${BASE_PATH}/log"
 
 export JAVA_OPT="-server -Xms256m -Xmx256m -Xmn512m -XX:MetaspaceSize=64m -XX:MaxMetaspaceSize=256m -XX:-OmitStackTraceInFastThrow"
 
