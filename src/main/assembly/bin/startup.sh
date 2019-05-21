565999f2b7b0799bb9a6e40cc7604c7e18eb280a
qutl <qutl@nextop.cn>
2019-05-21 11:55:45 +0800

    z

--- src/main/assembly/bin/startup.sh
+++ src/main/assembly/bin/startup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-export BASE_PATH=/app/apple
-export APPLICATION="apple"
-export APPLICATION_JAR="${APPLICATION}-0.0.1-SNAPSHOT.jar"
-export CONFIG_DIR=${BASE_PATH}/config
+export APP_HOME=/app/apple
+export APP_NAME="apple"
+export APP_NAME_JAR="${APP_NAME}-0.0.1-SNAPSHOT.jar"
+export CONFIG_DIR=${APP_HOME}/config
 
-export JAVA_OPT="-server -XX:+HeapDumpOnOutOfMemoryError -XX:+ExitOnOutOfMemoryError -XX:+AggressiveOpts -XX:+UseG1GC -XX:NewRatio=3 -verbose:gc -XX:+PrintGCCause -XX:+PrintGCDateStamps -XX:+PrintGCApplicationStoppedTime -Djava.net.preferIPv4Stack=true -XX:MaxDirectMemorySize=256M"
+export JAVA_OPT="-server -XX:+HeapDumpOnOutOfMemoryError -XX:+ExitOnOutOfMemoryError -XX:+AggressiveOpts -XX:+UseG1GC -XX:NewRatio=3 -verbose:gc -XX:+PrintGCCause -XX:+PrintGCDateStamps -XX:+PrintGCAPP_NAMEStoppedTime -Djava.net.preferIPv4Stack=true -XX:MaxDirectMemorySize=256M"
 
-nohup java ${JAVA_OPT} -jar ${BASE_PATH}/boot/${APPLICATION_JAR} --spring.config.location=${CONFIG_DIR} & >> ${BASE_PATH}/log/${APPLICATION}.out 2>&1
+nohup java ${JAVA_OPT} -jar ${APP_HOME}/boot/${APP_NAME_JAR} & >> ${APP_HOME}/log/${APP_NAME}.out 2>&1
