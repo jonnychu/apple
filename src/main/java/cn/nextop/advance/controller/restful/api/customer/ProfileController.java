94126e4195dd3eac20a2bf038a3cb26291d73750
qutl <qutl@nextop.cn>
2019-05-24 17:38:34 +0800

    z

--- src/main/java/cn/nextop/advance/controller/restful/api/customer/ProfileController.java
+++ src/main/java/cn/nextop/advance/controller/restful/api/customer/ProfileController.java
@@ -3,7 +3,7 @@ package cn.nextop.advance.controller.restful.api.customer;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
-import java.util.concurrent.Future;
+import java.util.concurrent.CompletableFuture;
 
 import org.springframework.http.MediaType;
 import org.springframework.scheduling.annotation.Async;
@@ -25,30 +25,38 @@ public class ProfileController extends AbstractCustomerController {
 	
 	@Async
 	@RequestMapping(value="/profile", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public Future<Map<String, Object>> getProfile(@RequestParam(name = "id", required = false) Integer id) {
-		Map<String, Object> resultData = new HashMap<>();
-		if (id == null) {
-			resultData.put("resultCode", "all"); return new AsyncResult<>(resultData);
-		}
-		switch (id) {
-		case 1:
-			resultData.put("resultCode", "profile 1");
-			break;
-		case 2:
-			resultData.put("resultCode", "profile 2");
-			break;
-		case 3:
-			resultData.put("resultCode", "profile 3");
-			break;
-		case 4:
-			resultData.put("resultCode", "profile 4");
-			break;
-		case 5:
-			throw new IllegalArgumentException();
-		default:
-			resultData.put("resultCode", UUID.randomUUID().toString());
-			break;
-		}
-		return new AsyncResult<>(resultData);
+	public CompletableFuture<Object> getProfile(@RequestParam(name = "id", required = false) Integer id) {
+		return CompletableFuture.supplyAsync(() -> {
+			
+			Map<String, Object> resultData = new HashMap<>();
+			if (id == null) {
+				resultData.put("resultCode", "all"); return new AsyncResult<>(resultData);
+			}
+			switch (id) {
+			case 1:
+				resultData.put("resultCode", "profile 1");
+				break;
+			case 2:
+				resultData.put("resultCode", "profile 2");
+				break;
+			case 3:
+				resultData.put("resultCode", "profile 3");
+				break;
+			case 4:
+				resultData.put("resultCode", "profile 4");
+				break;
+			case 5:
+				throw new IllegalArgumentException();
+			default:
+				resultData.put("resultCode", UUID.randomUUID().toString());
+				break;
+			}
+			try {
+				Thread.sleep(6000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			return resultData;
+		});
 	}
 }
