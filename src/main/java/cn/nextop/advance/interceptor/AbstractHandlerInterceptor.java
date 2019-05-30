24c0bff6cf49fcd26c0bdd786c3defc053a1220b
qutl <qutl@nextop.cn>
2019-05-30 19:23:32 +0800

    z

--- src/main/java/cn/nextop/advance/interceptor/AbstractHandlerInterceptor.java
+++ src/main/java/cn/nextop/advance/interceptor/AbstractHandlerInterceptor.java
@@ -1,6 +1,11 @@
 package cn.nextop.advance.interceptor;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.lang.Nullable;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * 
@@ -9,4 +14,13 @@ import org.springframework.web.servlet.HandlerInterceptor;
  */
 public abstract class AbstractHandlerInterceptor implements HandlerInterceptor {
 	
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		return true;
+	}
+
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+	}
+
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+	}
 }
--- src/main/java/cn/nextop/advance/interceptor/AbstractHandshakeInterceptor.java
+++ src/main/java/cn/nextop/advance/interceptor/AbstractHandshakeInterceptor.java
@@ -1,10 +1,14 @@
 package cn.nextop.advance.interceptor;
 
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.HandshakeInterceptor;
 
 /**
@@ -23,5 +27,14 @@ public abstract class AbstractHandshakeInterceptor implements HandshakeIntercept
 	
 	protected final HttpServletRequest getServletRequest(ServerHttpRequest request) {
 		return request instanceof ServletServerHttpRequest ? ((ServletServerHttpRequest) request).getServletRequest() : null;
+	}
+	
+	@Override
+	public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
+	}
+	
+	@Override
+	public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+		return true;
 	}
 }
